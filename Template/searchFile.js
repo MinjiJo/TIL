16c5372ec2e6ca3404b0f4e214d590e0c1be702d
Minji Jo <44547348+MinjiJo@users.noreply.github.com>
2021-08-05 18:01:39 +0900

    Update searchFile.js

--- Template/searchFile.js
+++ Template/searchFile.js
@@ -1,9 +1,14 @@
 function search(){
 	history.go(0);
+	return false;
 }
 function enterkey() {
 	if (window.event.keyCode == 13) {
 		// 엔터키가 눌렸을 때 실행할 내용
 		search();
 	}
+}
+function addFile() {
+	history.go(0);
+	return false;
 }
