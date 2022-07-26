1777fedc914f4ffdc087b2bcf6b1b328c2b7e272
MinjiJo <story3697@gmail.com>
2022-07-26 20:33:24 +0900

    Solving Algorithm

--- Algorithm/InflearnJavaAlgorithm/src/p1_8.java
+++ Algorithm/InflearnJavaAlgorithm/src/p1_8.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Main {
+public class p1_8 {
     public String solution(String s){
         //replaceAll 은 정규식 사용 가능. replace 는 불가능.
         String answer="NO";
@@ -11,7 +11,7 @@ public class Main {
     }
 
     public static void main(String[] args){
-        Main T = new Main();
+        p1_8 T = new p1_8();
         Scanner kb = new Scanner(System.in);
         String str = kb.nextLine();
         System.out.println(T.solution(str));        
