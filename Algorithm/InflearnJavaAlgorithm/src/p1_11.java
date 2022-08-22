a58122d2453af8aa9edd4f1df1d171825558ecf6
MinjiJo <story3697@gmail.com>
2022-08-22 22:39:46 +0900

    Solving Algorithm

---
+++ Algorithm/InflearnJavaAlgorithm/src/p1_11.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+public class p1_11 {
+    public String solution(String s){
+        //String 입력 받은 값에 빈칸 하나 넣는 것이 Key!!!
+        String answer="";
+        s=s+" ";
+        int cnt=1;
+        for(int i=0; i<s.length()-1; i++){
+            if(s.charAt(i)==s.charAt(i+1)) cnt++;
+            else {
+                answer+=s.charAt(i);
+                if(cnt>1) answer+=String.valueOf(cnt); //integer.toString() 써도 됨
+                cnt=1;
+            }
+        }
+
+        return answer;
+        /**내가 만든 코드
+        String answer="";
+        int a=1;
+        for(int i=0; i<s.length(); i++){
+            if(i!=s.length()-1){
+                if(s.charAt(i)==s.charAt(i+1)){
+                    a++;
+                }
+                else {
+                    answer+=s.charAt(i);
+                    if(a!=1) {
+                        answer+=a;
+                        a=1;
+                    }
+                }
+            } else {
+                answer+=s.charAt(i);
+                if(a!=1) {
+                    answer+=a;
+                }
+            }
+            
+        }
+
+        return answer;
+         **/
+    }
+
+    public static void main(String[] args){
+        p1_11 T = new p1_11();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.nextLine();
+        System.out.println(T.solution(str));        
+    }
+}
--- Algorithm/InflearnJavaAlgorithm/src/p1_12.java
+++ Algorithm/InflearnJavaAlgorithm/src/p1_12.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Main {
+public class p1_12 {
     public String solution(int n, String s){
         //substring 으로 7개씩 끊어내기
         //replace('바꿀것','바꾸고 싶은 값');
@@ -16,7 +16,7 @@ public class Main {
     }
 
     public static void main(String[] args){
-        Main T = new Main();
+        p1_12 T = new p1_12();
         Scanner kb = new Scanner(System.in);
         int n = kb.nextInt();
         String str = kb.next();
