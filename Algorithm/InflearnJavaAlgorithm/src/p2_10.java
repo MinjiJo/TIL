6aea69d33874004a365cc02b192a2462262197fd
MinjiJo <story3697@gmail.com>
2022-11-01 22:43:35 +0900

    Solving Algorithm

--- Algorithm/InflearnJavaAlgorithm/src/p2_10.java
+++ Algorithm/InflearnJavaAlgorithm/src/p2_10.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Main {
+public class p2_10 {
     int[] dx = {-1, 0, 1, 0};
     int[] dy = {0, 1, 0, -1};
 
@@ -24,7 +24,7 @@ public class Main {
     }
 
     public static void main(String[] args){
-        Main T = new Main();
+        p2_10 T = new p2_10();
         Scanner kb = new Scanner(System.in);
         int n = kb.nextInt();
         int[][] arr = new int[n][n];
