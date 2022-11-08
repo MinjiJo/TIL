a7c867ece180075642cb51e320340f66af8c04e8
MinjiJo <story3697@gmail.com>
2022-11-08 22:28:12 +0900

    Solving Algorithm

--- Algorithm/InflearnJavaAlgorithm/src/Main.java
+++ Algorithm/InflearnJavaAlgorithm/src/Main.java
@@ -1,12 +1,10 @@
 import java.util.*;
 
-import org.junit.Test;
-
 public class Main {
     public int solution(int n, int m, int[][] arr){
         int answer = 0;
         for(int i=1; i<=n; i++){
-            for(int j=1; j<=m; j++){
+            for(int j=1; j<=n; j++){
                 int cnt=0;
                 for(int k=0; k<m; k++){
                     int pi=0, pj=0;
