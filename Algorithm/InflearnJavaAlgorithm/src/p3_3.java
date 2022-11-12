5f2c86bc6b807ecd419f2b459d206167dbc1d728
MinjiJo <story3697@gmail.com>
2022-11-12 23:00:18 +0900

    Solving Algorithm

--- Algorithm/InflearnJavaAlgorithm/src/p3_3.java
+++ Algorithm/InflearnJavaAlgorithm/src/p3_3.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Main {
+public class p3_3 {
     public int solution(int n, int k, int[] arr){
         //window 방식으로 오른쪽으로 쭉쭉 밀어가면서 오른쪽을 더하고 가장 왼쪽을 빼는 방식으로 해야 함!
         //무조건 k만큼을 다 더하면 너무 횟수가 많아짐
@@ -16,7 +16,7 @@ public class Main {
     }
 
     public static void main(String[] args){
-        Main T = new Main();
+        p3_3 T = new p3_3();
         Scanner kb = new Scanner(System.in);
         int n=kb.nextInt();
         int k=kb.nextInt();
